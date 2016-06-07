06defb3c84de07abf06bf93b36adad362dea22df
Michael Sevilla <mikesevilla3@gmail.com>
2016-06-06 17:21:32 -0700

    baseliner: intergrate more fully with jenkins
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/baseliner/run.sh
+++ experiments/baseliner/run.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+rm ../*.tar.gz >> /dev/null 2>&1
+
 set -ex
 
-ANSIBLE_LOG_PATH="logs/radosbench.log" \
+ANSIBLE_LOG_PATH="logs/baseliner.log" \
 ansible-playbook --forks 50 --skip-tags "with_pkg" \
 ceph.yml \
   workloads/radosbench.yml \
   workloads/osdbench.yml \
   workloads/netbench.yml \
 collect.yml
+
+cd ../
+tar czf baseliner.tar.gz baseliner
+cd -
