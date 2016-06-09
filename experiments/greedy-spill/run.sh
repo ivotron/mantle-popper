bedec5337a252349b4a1a69d57c938460a31393a
Michael Sevilla <mikesevilla3@gmail.com>
2016-06-09 15:56:25 -0700

    expr: greedy spill setup and initial bench
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/greedy-spill/run.sh
+++ experiments/greedy-spill/run.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
-
-rm ../*.tar.gz >> /dev/null 2>&1
-
 set -ex
 
-ANSIBLE_LOG_PATH="logs/baseliner.log" \
-ansible-playbook --forks 50 --skip-tags "with_pkg" \
-ceph.yml 
-#\
-#  workloads/radosbench.yml \
-#  workloads/osdbench.yml \
-#  workloads/netbench.yml \
-#collect.yml
-#
-#cd ../
-#tar czf baseliner.tar.gz baseliner
+ANSIBLE_LOG_PATH="../results/logs/greedy-spill.log"
+WORKLOAD="../workloads"
+RUN="ansible-playbook --skip-tags with_pkg"
+
+cd site
+$RUN mantle.yml
+$RUN $WORKLOAD/mdtest.yml
+$RUN collect.yml
+cd -
+
+#tar czf mdtest.tar.gz baseliner
 #cd -
