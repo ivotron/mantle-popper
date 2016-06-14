d180b90479cbae16f54e60d225f612e85e1f9bfd
Michael Sevilla <mikesevilla3@gmail.com>
2016-06-14 15:38:11 -0700

    expr: figure out pathing
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/run.sh
+++ experiments/run.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
+set -ex
+
 root=`dirname $0`
 mydir=`pwd`
 
-rm $root/*.tar.gz >> /dev/null 2>&1
-set -ex
-
 for experiment in baseliner greedy-spill; do 
   cd $root/$experiment
-  ./run.sh
-  cd $mydir
+  #./run.sh
 done
+
+cd $mydir
