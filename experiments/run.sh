68045bcb0766286b78f02d55ee78aff1ad7072c3
Michael Sevilla <mikesevilla3@gmail.com>
2016-06-15 11:36:44 -0700

    expr: add long running experiment
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/run.sh
+++ experiments/run.sh
@@ -4,9 +4,10 @@ set -ex
 
 root=`pwd`
 
-for experiment in baseliner greedy-spill; do 
+#for experiment in baseliner greedy-spill; do 
+for experiment in greedy-spill; do 
   cd $root/$experiment
-  #./run.sh
+  ./run.sh
 done
 
 cd $root
