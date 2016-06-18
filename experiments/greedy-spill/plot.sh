2c4e71fd27b907e9fee0f52ab0c8a92b3a2be33c
Michael Sevilla <mikesevilla3@gmail.com>
2016-06-17 17:59:40 -0700

    expr: plot cpu and load
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/greedy-spill/plot.sh
+++ experiments/greedy-spill/plot.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
 rm results/*.png >> /dev/null 2>&1
+rm -r results/utilization/tmp results/utilization/*.out >> /dev/null 2>&1
 
 set -ex
+
 sudo chown -R ${USER}:${USER} .
+
+echo "plot: prepare CPU utilizations"
+cd results/utilization
+for i in `ls`; do 
+  tar xzf $i
+  node="${i%.tar.gz}"
+  echo "... dumping cpu utilization for $node"
+  docker run \
+    -v `pwd`/tmp:/tmp \
+    --entrypoint=whisper-dump.py \
+    michaelsevilla/graphite \
+    /tmp/graphite/whisper/$node/cpuload/avg1.wsp > cpu-$node.out
+done
+cd -
+
+echo "plot: construct actual graphs"
 docker run --rm \
   -v "`pwd`:/home/jovyan/work" \
   jupyter/scipy-notebook \
