974e78fbaaea088555a82362b059c71aebb1bd58
Michael Sevilla <mikesevilla3@gmail.com>
2016-06-17 18:45:12 -0700

    expr: re-add baseliner
    Signed-off-by: Michael Sevilla <mikesevilla3@gmail.com>

--- experiments/run.sh
+++ experiments/run.sh
@@ -4,8 +4,7 @@ set -ex
 
 root=`pwd`
 
-#for experiment in baseliner greedy-spill; do 
-for experiment in greedy-spill; do 
+for experiment in baseliner greedy-spill; do 
   cd $root/$experiment
   ./run.sh
 done
